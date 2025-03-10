3175e6de12c0ea3d2c6693f49ba4b48d75403059
Charles Crossley <charlesdcrossley@gmail.com>
2025-03-10 11:32:10 -0500

    Added Tests for File Reader

---
+++ Algorithms.Tests/FileReaderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Test
+{
+    [TestClass]
+    public class FileReaderTests
+    {
+        [TestMethod]
+        public void ReadIntegersFromFile_WhenPassedValidIntegersFromFile_ReturnsIEnumerableWithIntegers()
+        {
+            var outputIntegers = FileReader.ReadIntegersFromFile("1Kints.txt");
+
+            Assert.IsTrue(outputIntegers.All(x => x is int));
+
+        }
+        [TestMethod]
+        public void ReadIntegersFromFile_WhenPassedNonIntegersFromFile_SkipsLine()
+        {
+            var outputIntegers = FileReader.ReadIntegersFromFile("1KintsNotInt.txt");
+
+            Assert.IsTrue(outputIntegers.All(x => x is int));
+        }
+
+
+        //
+
+
+    }
+}
---
+++ Algorithms/FileReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class FileReader
+    {
+        public static IEnumerable<int> ReadIntegersFromFile(string filePath)
+        {   
+            string line = string.Empty;
+
+            using (TextReader reader = new StreamReader(filePath))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if(int.TryParse(line, out int value))
+                    {
+                        yield return value;
+                    }
+
+                }
+            }
+        }
+    }
+}
