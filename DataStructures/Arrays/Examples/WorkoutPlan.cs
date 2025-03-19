3b9b46b67f11c407e0cf16d58c2b9b052acaa2de
Charles Crossley <charlesdcrossley@gmail.com>
2025-03-18 20:28:24 -0500

    Added preliminary Workout Plan using Jagged arrays

---
+++ DataStructures/Arrays/Examples/WorkoutPlan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays.Examples
+{
+    public class WorkoutPlan
+    {
+        public enum WorkoutType { Run, Bike, Swim}
+
+        public static WorkoutType[] GenerateWorkoutPlan()
+        {
+            Random random = new();
+            int workoutCount = Enum.GetNames<WorkoutType>().Length;
+
+            return [WorkoutType.Run, WorkoutType.Swim];
+
+        }
+
+    }
+}
