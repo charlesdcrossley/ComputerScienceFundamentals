4b5737472c3029c8d9bccbcbedcc475eb634e1a2
Charles Crossley <charlesdcrossley@gmail.com>
2025-03-31 17:44:48 -0500

    Selection Sort implementation

---
+++ Algorithms.Tests/Sorting/SelectionSortTests.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms.Sorting.SelectionSort;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Test.Sorting
+{
+    [TestClass]
+    public class SelectionSortTests
+    {
+
+        [TestMethod]
+        public void Sort_WhenGivenArrayOfIntegers_ReturnsSortedArray()
+        {
+            // Arrange
+            Random rand = new Random();
+            int[] unsortedArray = new int[10] { 4, 6, 1, 3, 2, 9, 7, 5, 8,10 };
+            int[] expectedResult = new int[10] { 1,2,3,4,5,6,7,8,9,10 };
+
+
+            // Act 
+            int[] sortedArray = SelectionSort.Sort(unsortedArray);
+
+            // Assert
+
+            CollectionAssert.Equals(sortedArray, expectedResult);
+        }
+    }
+}
---
+++ Algorithms/Sorting/SelectionSort/SelectionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting.SelectionSort
+{
+    public class SelectionSort
+    {
+        public static int[] Sort(int[] arrayOfIntegers)
+        {
+            for(int i=0; i<arrayOfIntegers.Length -1 ; i++)
+            {
+                int swapIndex = i;
+                int swapValue = arrayOfIntegers[i];
+                for (int j=i + 1; j<arrayOfIntegers.Length-1; j++)
+                {                    
+                    if (arrayOfIntegers[j] < swapValue)
+                    {
+                        swapIndex = j;
+                        swapValue = arrayOfIntegers[j];
+                    }
+                }
+                arrayOfIntegers  = swapValues(arrayOfIntegers, i, swapIndex);
+                
+            }
+            return arrayOfIntegers;
+        }
+        private static int[] swapValues(int[] array, int indexA, int indexB)
+        {
+            int temp = array[indexA];
+            array[indexA] = array[indexB];
+            array[indexB] = temp;
+            return array;
+        }
+    }
+}
