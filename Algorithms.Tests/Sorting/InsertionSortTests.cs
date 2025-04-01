5d0343b2fd9880ca24ca57aa8f56272f7c4add02
Charles Crossley <charlesdcrossley@gmail.com>
2025-03-31 21:10:43 -0500

    Added Insertion Sort Implementation

--- Algorithms.Tests/Sorting/InsertionSortTests.cs
+++ Algorithms.Tests/Sorting/InsertionSortTests.cs
@@ -1,4 +1,5 @@
-﻿using Algorithms.Sorting.SelectionSort;
+﻿using Algorithms.Sorting.InsertionSort;
+using Algorithms.Sorting.SelectionSort;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,21 +9,21 @@ using System.Threading.Tasks;
 namespace Algorithms.Test.Sorting
 {
     [TestClass]
-    public class SelectionSortTests
+    public class InsertionSortTests
     {
-
         [TestMethod]
         public void Sort_WhenGivenArrayOfIntegers_ReturnsSortedArray()
         {
             // Arrange
-            int[] unsortedArray = new int[10] { 4, 6, 1, 3, 2, 9, 7, 5, 8,10 };
-            int[] expectedResult = new int[10] { 1,2,3,4,5,6,7,8,9,10 };
+            int[] unsortedArray = new int[10] { 4, 6, 1, 3, 2, 9, 7, 5, 8, 10 };
+            int[] expectedResult = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             // Act 
-            int[] sortedArray = SelectionSort.Sort(unsortedArray);
+            int[] sortedArray = InsertionSort.Sort(unsortedArray);
 
             // Assert
             CollectionAssert.Equals(sortedArray, expectedResult);
+
         }
     }
 }
---
+++ Algorithms/Sorting/InsertionSort/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting.InsertionSort
+{
+    public class InsertionSort
+    {
+        public static int[] Sort(int[] unsortedArray)
+        {
+            for (int i = 0; i < unsortedArray.Length; i++)
+            {
+                int j = i;
+                while(j > 0 && unsortedArray[j] < unsortedArray[j-1])
+                {
+                    unsortedArray = swapValues(unsortedArray, j, j-1);
+                    j--;
+                }
+            }
+            return unsortedArray;
+        }
+        private static int[] swapValues(int[] array, int indexA, int indexB)
+        {
+            int temp = array[indexA];
+            array[indexA] = array[indexB];
+            array[indexB] = temp;
+            return array;
+        }
+    }
+}
